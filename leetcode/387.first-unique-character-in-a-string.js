c245f3f22ec8ba6dd46ba1c47eb185f965c1b07f
wurui <wurui_meetyou@163.com>
2019-07-04 22:21:22 +0800

    [leetcode 387 第一个不重复的字符 完成 ]

---
+++ leetcode/387.first-unique-character-in-a-string.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=387 lang=javascript
+ *
+ * [387] First Unique Character in a String
+ *
+ * https://leetcode.com/problems/first-unique-character-in-a-string/description/
+ *
+ * algorithms
+ * Easy (50.26%)
+ * Likes:    1055
+ * Dislikes: 79
+ * Total Accepted:    281.6K
+ * Total Submissions: 560.2K
+ * Testcase Example:  '"leetcode"'
+ *
+ * 
+ * Given a string, find the first non-repeating character in it and return it's
+ * index. If it doesn't exist, return -1.
+ * 
+ * Examples:
+ * 
+ * s = "leetcode"
+ * return 0.
+ * 
+ * s = "loveleetcode",
+ * return 2.
+ * 
+ * 
+ * 
+ * 
+ * Note: You may assume the string contain only lowercase letters.
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  // 映射
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      return i
+    }
+  }
+  return -1;
+};
+

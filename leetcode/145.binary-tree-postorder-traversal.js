598879fbbb37821831e6ad5bb0d63cf73e60ae57
wurui <wurui_meetyou@163.com>
2019-07-15 08:38:29 +0800

    [leetcode 145 后序遍历 完成  ]

---
+++ leetcode/145.binary-tree-postorder-traversal.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=145 lang=javascript
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Hard (48.98%)
+ * Likes:    958
+ * Dislikes: 48
+ * Total Accepted:    271.1K
+ * Total Submissions: 551.3K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [3,2,1]
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  if (!root) return [];
+  let post = []
+  postOrder(root)
+  function postOrder(root) {
+    if (root) {
+      postOrder(root.left)
+      postOrder(root.right)
+      post.push(root.val)
+    }
+  }
+  return post;
+};
+

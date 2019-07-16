03c02fc7c192c697cbc02d5db55b6c7818191038
wurui <wurui_meetyou@163.com>
2019-07-16 21:46:25 +0800

    [leetcode 219 数组重复数字下标小于k 完成 ]

---
+++ leetcode/219.contains-duplicate-ii.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=219 lang=javascript
+ *
+ * [219] Contains Duplicate II
+ *
+ * https://leetcode.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (35.49%)
+ * Likes:    519
+ * Dislikes: 636
+ * Total Accepted:    204.6K
+ * Total Submissions: 575.8K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * Given an array of integers and an integer k, find out whether there are two
+ * distinct indices i and j in the array such that nums[i] = nums[j] and the
+ * absolute difference between i and j is at most k.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  let data = {}
+  for (let i = 0; i < nums.length; i++) {
+    let idx = data[nums[i]];
+    if (idx !== undefined && i - idx <= k) {
+      return true
+    } else {
+      data[nums[i]] = i;
+    }
+  }
+  return false;
+};
+

946c55fdc78e276ed9c155a5d03517065b975722
wurui <wurui_meetyou@163.com>
2019-07-16 17:12:25 +0800

    [ lodash curry  ]

--- miao/lodash/wu-rui-lodash.js
+++ miao/lodash/wu-rui-lodash.js
@@ -2,4 +2,16 @@ var wu_rui = {
   compact: function (ary) {
     return ary.filter(it => it)
   },
+  curry: function (f, n = f.length) {
+    let ary = [];
+    return function (...arg) {
+      ary.push(...arg)
+      if (ary.length < n) {
+        return f;
+      } else {
+        return f(...ary.slice(0, n))
+      }
+    }
+  },
+
 }

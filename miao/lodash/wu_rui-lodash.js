6836425abb67cb5af682ba8df22d35d3d3464936
wurui <wurui_meetyou@163.com>
2019-07-28 21:38:33 +0800

    [lodash foreachRight ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -27,6 +27,24 @@ var wu_rui = function () {
     return collection;
   }
 
+  /**
+ * 从右到左遍历数组，即为逆向遍历
+ * 返回：原本的集合
+ */
+  function forEachRight(collection, iteratee = null) {
+    if (iteratee == null) return collection;
+    let right = []
+    for (let key in collection) {
+      right.push(key)
+    }
+    for (let i = right.length - 1; i > -1; i--) {
+      let val = collection[right[i]]
+      let key = right[i]
+      iteratee(val, key, collection)
+    }
+    return collection;
+  }
+
   function dropRight(array, n = 1) {
     if (n >= array.length) return [];
     return array.slice(0, array.length - n)
@@ -181,6 +199,7 @@ var wu_rui = function () {
     dropRight,
     fill,
     forEach,
+    forEachRight,
   }
 }()
 

a6b94f565807525ec01415363eb7a0d192e4a887
wurui <wurui_meetyou@163.com>
2019-07-28 22:55:42 +0800

    [lodash groupby ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -183,6 +183,32 @@ var wu_rui = function () {
     }
   }
 
+  /**
+   * 创建一个对象
+   * key 是 iteratee 遍历 collection(集合) 中的每个元素返回的结果
+   * 分组值的顺序是由他们出现在 collection(集合) 中的顺序确定的
+   * 每个键对应的值负责生成 key 的元素组成的数组。iteratee 调用 1 个参数：(value)
+   * 这里还需要理解传入的参数的iteratee为数组和对象的情况，但是没有例子，无法理解
+   */
+  function groupBy(collection, iteratee = null) {
+    let res = {}
+    let key = null;
+    // TODO： 这里需要判断四种情况，obejct，function，array，string
+    let type = typeof iteratee;
+    if (iteratee instanceof Array) {
+      type = 'array'
+    }
+    for (let val of collection) {
+      key = type == 'string' ? val[iteratee] : iteratee(val)
+      if (!res[key]) {
+        res[key] = [val]
+      } else {
+        res[key].push(val)
+      }
+    }
+    return res;
+  }
+
   return {
     compact,
     curry,
@@ -200,6 +226,7 @@ var wu_rui = function () {
     fill,
     forEach,
     forEachRight,
+    groupBy,
   }
 }()
 

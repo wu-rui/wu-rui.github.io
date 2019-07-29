d624e945787b845163cf56a806fb847c538f3bdc
wurui <wurui_meetyou@163.com>
2019-07-29 16:35:54 +0800

    [lodash dropRightWhile ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -50,6 +50,58 @@ var wu_rui = function () {
     return array.slice(0, array.length - n)
   }
 
+
+  /**
+   * 这里理解题意很重要
+   * 从右往左遍历，需要去掉当遇到第一个false的时候的所有后面的数据，返回第一个false的位置的内容
+   * @param {Array} array 
+   * @param {Function/Object/Array/String} param1 
+   * 因为数组和对象是一样的遍历方式，所以不需要判断是否是数组
+   */
+  function dropRightWhile(array, predicate = null) {
+    if (predicate == null) return array;
+    let n = array.length;
+    let type = typeof predicate;
+    if (predicate instanceof Array) type = 'array'
+    for (let i = n - 1; i > -1; i--) {
+      let val = array[i]
+      let cur = true;
+      switch (type) {
+        case 'string':
+          cur = val[predicate]
+          break;
+        case 'function':
+          cur = predicate(val, i, array)
+          break;
+        case 'object':
+          for (let v in predicate) {
+            if (val[v] !== predicate[v]) {
+              cur = false;
+              break;
+            }
+          }
+          break;
+        case 'array':
+          for (let v = 0; v < predicate.length; v++) {
+            if (predicate[v + 1]) {
+              if (val[predicate[v]] !== predicate[v + 1]) {
+                cur = false;
+                break;
+              }
+            }
+          }
+          break;
+        default:
+          break;
+      }
+      if (cur == false) {
+        n = i;
+        break;
+      }
+    }
+    return array.slice(0, n + 1);
+  }
+
   /**
    * 
    * @param {Array} ary 
@@ -135,6 +187,11 @@ var wu_rui = function () {
     }
   }
 
+  /**
+   * 记忆化函数
+   * @param {Function} f
+   *  
+   */
   function memoize(f) {
     let cache = {}
     return function (arg) {
@@ -145,6 +202,19 @@ var wu_rui = function () {
       }
     }
   }
+  // function memoize(f) {
+  //   let cache = new Map()
+  //   function memoized(arg) {
+  //     if (cache.has(arg)) {
+  //       return cache.get(arg)
+  //     } else {
+  //       cache.set(arg, f(arg))
+  //       return cache.get(arg)
+  //     }
+  //   }
+  //   memoized.cache = cache;
+  //   return memoized;
+  // }
 
   function ary(func, n = func.length) {
     return function (...args) {
@@ -227,6 +297,7 @@ var wu_rui = function () {
     forEach,
     forEachRight,
     groupBy,
+    dropRightWhile,
   }
 }()
 

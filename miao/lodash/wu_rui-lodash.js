13a2f0b2358b7511916f8395c7562439e5172613
wurui <wurui_meetyou@163.com>
2019-07-29 22:33:48 +0800

    [lodash findIndex ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -154,6 +154,103 @@ var wu_rui = function () {
     return array.slice(n);
   }
 
+
+  /**
+ * 找到第一个比较为true的数据的下标
+ * @param {*} array 
+ * @param {*} predicate 
+ * @param {*} fromIndex 
+ */
+  function findIndex(array, predicate = null, fromIndex = 0) {
+    if (predicate == null) return 0;
+    let res = -1;
+    let type = getType(predicate)
+    for (let i = fromIndex; i < array.length; i++) {
+      let val = array[i]
+      let cur = false;
+      switch (type) {
+        case 'function':
+          cur = predicate(val)
+          break;
+        case 'string':
+          return findIndex(array, property.bind(null, predicate), fromIndex)
+        case 'array':
+          return findIndex(array, matchesProperty.bind(null, predicate[0], predicate[1]), fromIndex)
+        case 'object':
+          return findIndex(array, matches.bind(null, predicate), fromIndex)
+        default:
+          break;
+      }
+      if (cur) {
+        res = i;
+        break;
+      }
+    }
+    return res;
+  }
+
+
+  /**
+   * 返回传入值的原型类型
+   * array，string，object，function
+   * @param {*} val 
+   */
+  function getType(val) {
+    let type = typeof val;
+    if (val instanceof Array) type = 'array';
+    return type;
+  }
+
+  /**
+   * 对比val和obj是否相同，返回一个函数
+   * bind的时候需要绑定第一个参数
+   * @param {*} obj 
+   * @returns {Function}
+   */
+  function matches(obj, val) {
+    let res = true;
+    for (let v in obj) {
+      if (obj[v] !== val[v]) {
+        res = false;
+        break;
+      }
+    }
+    return res;
+  }
+
+  /**
+   * 当对比的数据中出现键值对的时候
+   * 需要对比数据是否符合key，val
+   * 返回true，false
+   * bind的时候，需要绑定前两个参数
+   * @param {} key 
+   * @param {*} val 
+   * @param {*} data 
+   */
+  function matchesProperty(key, val, data) {
+    return data[key] == val
+  }
+
+
+  /**
+   * 判断给定的字符串中的每一个属性对应到val数据中，是否为true
+   * 返回true，false
+   * bind的时候，需要绑定第一个参数
+   * @param {String} str 
+   * @param {Object} val 
+   */
+  function property(str, val) {
+    str = str.split('.');
+    for (let i of str) {
+      val = val[i]
+    }
+    return !(val === false || val == undefined);
+  }
+
+
+
+
+
   /**
    * 
    * @param {Array} ary 
@@ -434,6 +531,7 @@ var wu_rui = function () {
     differenceBy,
     differenceWith,
     dropWhile,
+    findIndex,
   }
 }()
 

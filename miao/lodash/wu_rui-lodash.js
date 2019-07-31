607ca0a6e5fa7e7bcd7be95a9e22e6197d1c87f9
wurui <wurui_meetyou@163.com>
2019-07-31 10:18:31 +0800

    [lodash findLastIndex ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -190,6 +190,43 @@ var wu_rui = function () {
   }
 
 
+
+
+  /**
+   * 和findIndex类似，只不过从后面往前遍历
+   * @param {*} array 
+   * @param {*} predicate 
+   * @param {*} fromIndex 
+   */
+  function findLastIndex(array, predicate = null, fromIndex = array.length - 1) {
+    if (predicate == null) return 0;
+    let res = -1;
+    let type = getType(predicate)
+    for (let i = fromIndex; i > -1; i--) {
+      let val = array[i]
+      let cur = false;
+      switch (type) {
+        case 'function':
+          cur = predicate(val)
+          break;
+        case 'string':
+          return findLastIndex(array, property(predicate), fromIndex)
+        case 'array':
+          return findLastIndex(array, matchesProperty(predicate[0], predicate[1]), fromIndex)
+        case 'object':
+          return findLastIndex(array, matches(predicate), fromIndex)
+        default:
+          break;
+      }
+      if (cur) {
+        res = i;
+        break;
+      }
+    }
+    return res;
+  }
+
+
   /**
    * 返回传入值的原型类型
    * array，string，object，function
@@ -532,73 +569,6 @@ var wu_rui = function () {
     differenceWith,
     dropWhile,
     findIndex,
+    findLastIndex,
   }
 }()
-
-// var test = function () {
-//   function negate(f) {
-//     return function (...args) {
-//       return !f(...args);
-//     }
-//   }
-//   // 等同于
-//   var negate = f => (...args) => !f(...args);
-
-//   // lodash的flip函数
-//   function flip(func) {
-//     return function (...args) {
-//       return func(...args.reverse())
-//     }
-//   }
-//   console.gol = flip(console.log)
-//   console.gol(123) == 321
-//   /**
-//    *
-//    * 返回一个函数，在前n次被调用的时候调用的是原函数
-//    * 当超过n次直接返回最后一次调用的结果
-//    * @param {*} m
-//    * @param {*} func
-//    */
-
-//   function before(n, func) {
-//     var times = 0;
-//     var lastRes = null;
-//     return function (...args) {
-//       times++;
-//       if (times < n) {
-//         return lastRes = func(...args)
-//       } else {
-//         return lastRes;
-//       }
-//     }
-//   }
-
-
-
-//   function f() {
-//     return this.val;
-//   }
-//   obj = {
-//     f: f,
-//     val: '哈哈哈',
-//   }
-//   obj.f()
-//   等同于
-//   f.apply(obj)
-//   a = f.appl
-//   a.apply(f)
-//   f.apply.apply(f, [obj])
-
-
-
-
-//   ary = [1, 2, 3, 4]
-//   ary.slice = function (start, end) {
-//     let res = []
-//     for (let i = start; i < end; i++) {
-//       console.log(this)
-//       res.push(this[i])
-//     }
-//     return res;
-//   }
-// }

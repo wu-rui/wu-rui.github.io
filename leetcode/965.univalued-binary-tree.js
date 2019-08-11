167f82c462e137d9b8d6702dbd6a22e1bb3d6df5
wurui <wurui_meetyou@163.com>
2019-08-11 16:12:26 +0800

    [ leetcode  965 判断树的值是否都相等 ]

---
+++ leetcode/965.univalued-binary-tree.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=965 lang=javascript
+ *
+ * [965] Univalued Binary Tree
+ *
+ * https://leetcode.com/problems/univalued-binary-tree/description/
+ *
+ * algorithms
+ * Easy (66.93%)
+ * Likes:    216
+ * Dislikes: 36
+ * Total Accepted:    42.7K
+ * Total Submissions: 63.8K
+ * Testcase Example:  '[1,1,1,1,1,null,1]'
+ *
+ * A binary tree is univalued if every node in the tree has the same value.
+ * 
+ * Return true if and only if the given tree is univalued.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,1,1,1,1,null,1]
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [2,2,2,5,2]
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The number of nodes in the given tree will be in the range [1, 100].
+ * Each node's value will be an integer in the range [0, 99].
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function (root) {
+  let val = root.val
+  function mapTree(root) {
+    if (root == null) return true;
+    if (root.val !== val) return false;
+    return mapTree(root.left) && mapTree(root.right)
+  }
+  return mapTree(root)
+};
+

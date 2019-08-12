974d4a30805d1861e9841967c5d4b00b528a271c
wurui <wurui_meetyou@163.com>
2019-08-12 21:42:44 +0800

    [ leetcode 350 两个数组的交集 ]

---
+++ leetcode/350.intersection-of-two-arrays-ii.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=350 lang=javascript
+ *
+ * [350] Intersection of Two Arrays II
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (48.52%)
+ * Likes:    764
+ * Dislikes: 271
+ * Total Accepted:    223K
+ * Total Submissions: 459.3K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two arrays, write a function to compute their intersection.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2,2]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [4,9]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Each element in the result should appear as many times as it shows in both
+ * arrays.
+ * The result can be in any order.
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * What if the given array is already sorted? How would you optimize your
+ * algorithm?
+ * What if nums1's size is small compared to nums2's size? Which algorithm is
+ * better?
+ * What if elements of nums2 are stored on disk, and the memory is limited such
+ * that you cannot load all elements into the memory at once?
+ * 
+ * 
+ */
+/**
+ * nums1 = [1,2,2,1], nums2 = [2,2]
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  let res = []
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] == nums2[j]) {
+        res.push(nums1[i])
+        nums1[i] = null;
+        nums2[j] = null;
+        break;
+      }
+    }
+  }
+  return res;
+};
+

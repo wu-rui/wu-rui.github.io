0028f3d6093754823f814c568d71fb923d4f628a
wurui <wurui_meetyou@163.com>
2019-08-12 11:01:00 +0800

    [ lodash  isequal 数组和对象的漏洞 ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -837,10 +837,11 @@ var wu_rui = function () {
    * @test isEqual({},{}) =>true
    * @test isEqual([1],[2]) =>false
    * @test isEqual({'a':1,'b':2},{'b'：2}) =>true
+   * @test isEqual([1,2],{"0":1,"1":2,"length":2})
    */
   function isEqual(obj1, obj2) {
     if (obj1 === obj2) return true;
-    if (typeof obj1 === typeof obj2) {
+    if (typeof obj1 === typeof obj2 && Array.isArray(obj1) == Array.isArray(obj2)) {
       if (typeof obj1 == 'object' && typeof obj2 == 'object') {
         for (let i in obj1) {
           if (typeof obj1[i] == 'object' && typeof obj2[i] == 'object') {

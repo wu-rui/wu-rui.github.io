f513aa8a2eb59a95a4b870b14b1303a3b4afd400
wurui <wurui_meetyou@163.com>
2019-08-12 15:43:00 +0800

    [ lodash  intersection   求多个数组的交集 ]

--- miao/lodash/wu_rui-lodash.js
+++ miao/lodash/wu_rui-lodash.js
@@ -901,7 +901,33 @@ var wu_rui = function () {
     }
   }
 
+
+
+  /**
+   * 求所有数组的并集(不重复的并集)
+   * @param  {Arrays} array 
+   * @test intersection([1,2,3,4,3],[3,4,3,5,7])=>[3, 4]
+   */
+  function intersection(...array) {
+    let cur = array[0]
+    if (array.length > 1) {
+      array.slice(1).forEach(eachAry => {
+        cur = cur.length == 0 ? '' : hasSameData(cur, eachAry)
+      });
+    }
+    return cur;
+  }
+
+  function hasSameData(cur, ary) {
+    let res = []
+    let data = {}
+    for (let val of cur) data[val] = val
+    for (let a of ary) if (data[a] && !res.includes(a)) res.push(a)
+    return res;
+  }
+
   return {
+    intersection,
     isEqual,
     isMatch,
     pullAll,

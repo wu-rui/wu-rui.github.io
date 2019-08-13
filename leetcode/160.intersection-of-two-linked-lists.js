afa6787d6c6b09c9a28b6cb514b57f50c5c0dbd0
wurui <wurui_meetyou@163.com>
2019-08-13 21:50:05 +0800

    [ leetcode 160 相交链表题 一定要重做一次 ]

---
+++ leetcode/160.intersection-of-two-linked-lists.js
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode id=160 lang=javascript
+ *
+ * [160] Intersection of Two Linked Lists
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (34.97%)
+ * Likes:    2310
+ * Dislikes: 216
+ * Total Accepted:    329.7K
+ * Total Submissions: 941.2K
+ * Testcase Example:  '8\n[4,1,8,4,5]\n[5,0,1,8,4,5]\n2\n3'
+ *
+ * Write a program to find the node at which the intersection of two singly
+ * linked lists begins.
+ * 
+ * For example, the following two linked lists:
+ * 
+ * 
+ * begin to intersect at node c1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA =
+ * 2, skipB = 3
+ * Output: Reference of the node with value = 8
+ * Input Explanation: The intersected node's value is 8 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2
+ * nodes before the intersected node in A; There are 3 nodes before the
+ * intersected node in B.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3,
+ * skipB = 1
+ * Output: Reference of the node with value = 2
+ * Input Explanation: The intersected node's value is 2 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [0,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes
+ * before the intersected node in A; There are 1 node before the intersected
+ * node in B.
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * 
+ * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB =
+ * 2
+ * Output: null
+ * Input Explanation: From the head of A, it reads as [2,6,4]. From the head of
+ * B, it reads as [1,5]. Since the two lists do not intersect, intersectVal
+ * must be 0, while skipA and skipB can be arbitrary values.
+ * Explanation: The two lists do not intersect, so return null.
+ * 
+ * 
+ * 
+ * 
+ * Notes:
+ * 
+ * 
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function
+ * returns.
+ * You may assume there are no cycles anywhere in the entire linked
+ * structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * 经典的相交链表题
+ * 使用双指针，从ab分别开始往后遍历
+ * 遍历完，a的指针就指向b的头结点，b一样
+ * 如果是相交节点，尾部节点的数据一定相同，继续下去一定会相遇
+ * 如果不是相交节点，尾部节点的数据一定不同
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (headA == null || headB == null) return null;
+  let pA = headA;
+  let pB = headB;
+  while (pA != pB) {
+    pA = pA == null ? headB : pA.next;
+    pB = pB == null ? headA : pB.next;
+  }
+  return pA
+};
+
+

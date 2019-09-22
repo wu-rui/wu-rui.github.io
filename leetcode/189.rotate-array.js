388258672e4f9d0dd4e1c55992e34f25d797eead
wurui <wurui_meetyou@163.com>
2019-09-22 21:52:02 +0800

    [ leetcode 189 完成 ]

---
+++ leetcode/189.rotate-array.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (30.98%)
+ * Likes:    1516
+ * Dislikes: 633
+ * Total Accepted:    328.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation: 
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  let n = nums.length;
+  k = k % n
+  if (k == 0) return
+  let newAry = [...nums]
+  nums.forEach((it, i) => {
+    nums[i] = i < k ? newAry[i + n - k] : newAry[i - k]
+  })
+};
